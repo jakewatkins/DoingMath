40fc8e436aaee5e1d5f304867dae3d27d8916af6
jkw <jake.watkins@gmail.com>
2021-01-30 20:53:25 -0600

    update for app store

---
+++ doingmath-icons/makefiles.sh
@@ -0,0 +1,46 @@
+#
+# make icons for the iphone and watch apps
+#
+
+#phone icons
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 40x40! -density 20 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/phone/phone-not-40.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 60x60! -density 29 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/phone/phone-not-60.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 58x58! -density 40 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/phone/phone-set-58.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 87x87! -density 60 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/phone/phone-set-87.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 80x80! -density 20 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/phone/phone-spot-80.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 120x120! -density 40 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/phone/phone-spot-120.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 120x120! -density 60 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/phone/phone-app-120.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 180x180! -density 60 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/phone/phone-app-180.png
+
+#store
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 1024x1024! -density 1024 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/phone/store-1024.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 1024x1024! -density 1024 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/watch/store-1024.png
+
+#ipad
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 20x20! -density 20 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/ipad/ipad-not-20.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 40x40! -density 20 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/ipad/ipad-not-40.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 29x29! -density 29 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/ipad/ipad-set-29.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 58x58! -density 29 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/ipad/ipad-set-58.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 40x40! -density 40 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/ipad/ipad-spot-40.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 80x80! -density 40 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/ipad/ipad-spot-80.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 76x76! -density 76 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/ipad/ipad-app-76.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 152x152! -density 76 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/ipad/ipad-app-152.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 167x167! -density 83.5 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/ipad/ipad-app-167.png
+
+
+#watch icons
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 48x48! -density 24 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/watch/watch-not-48.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 55x55! -density 24 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/watch/watch-not-55.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 58x58! -density 29 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/watch/watch-set-58.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 87x87! -density 29 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/watch/watch-set-87.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 80x80! -density 40 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/watch/watch-home-80.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 88x88! -density 44 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/watch/watch-home-88.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 100x100! -density 50 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/watch/watch-home-100.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 172x172! -density 86 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/watch/watch-short-172.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 196x196! -density 98 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/watch/watch-short-196.png
+magick ~/SourceCode/GP/DoingMath/doingmath-icons/Weight-Lifting-Icon-PNG.png -resize 216x216! -density 108 -units PixelsPerInch ~/SourceCode/GP/DoingMath/doingmath-icons/watch/watch-short-216.png
+
+
+
+
+
